98c1e22b4297c830b10ec0c31312e782f3c2536c
HankWang <xihongw103@gmail.com>
2024-12-14 16:57:57 +0800

    遊戲主畫面修改

--- source/javascript/Start.js
+++ source/javascript/Start.js
@@ -1,3 +1,4 @@
+//轉場的javascript
 document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('Gamestart_Button');
     const transitionBlock = document.getElementById('TransitionBlock');
@@ -13,4 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
             location.href = "./source/html/Game.html";
         }, 300); // 與 CSS transition 的時間一致
     });
+});
+
+// 取得影片元素和淡入元素
+const video = document.getElementById('myVideo');
+const fadeContent = document.getElementById('fadeContent');
+
+// 當影片播放結束時執行
+video.addEventListener('ended', function () {
+    // 影片播放結束後添加 active 類別，觸發淡入效果
+    if (fadeContent) {
+        fadeContent.classList.add('active');
+    }
 });
